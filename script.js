12441bff6602fd73180e4d97a71404d6469136bf
ompatel33 <ompatel@unc.edu>
2025-03-06 09:50:39 -0500

    add vector details

--- script.js
+++ script.js
@@ -7,54 +7,34 @@ L.tileLayer('https://cawm.lib.uiowa.edu/tiles/{z}/{x}/{y}.png', {}).addTo(map);
  
 //add geoJSON data
 fetch("attendance-geodata.geojson")
-.then(response => {
-    if (!response.ok) return;
-    return response.json();
+    .then(response => {
+        if (!response.ok) return;
+        return response.json();
 })
-.then(raw_data => {
-    L.geoJSON(raw_data).addTo(map);
-})
+    .then(raw_data => {
+        L.geoJSON(raw_data, {
+            //add vector appearance
+            pointToLayer: function(feature, latlng) {
+                if (feature.properties.attendance == true) { //HELP! How do I reference all attendance when they have different names!!
+                }
+                return L.circleMarker(latlng, {
+                    radius : 5,         //set size adjustment
+                    color : "black",    //set outline color
+                    fillColor : "black",//set fill color
+                    fillOpacity : 0.5,  //set transparency
+                    weight : 1          //set outline thickness   
+                });
+            }, 
+            //add popups
+            onEachFeature: function(feature, layer) {  
+                //customize popup content
+                var popupContent = 
+                    `<b>${feature.properties.See}</b><br>
+                    Province: ${feature.properties.Province || "Unknown"}<br>
+                    Notes: ${feature.properties.Notes || "None"}`;
+                layer.bindPopup(popupContent);
+        }}).addTo(map);
+    })
 .catch(error => {
     console.error(error);
 })
- 
-//add vector appearance (point)
-const bishoprics = [
-    { name: "Felix", lat: 37.299552, lon: -3.137125, attended: true },
-];
-
-bishoprics.forEach((bishopric) => {
-    L.circleMarker([bishopric.lat, bishopric.lon], {
-        color: bishopric.attended ? "green" : "red", // Green if attended, red if not
-        fillColor: bishopric.attended ? "green" : "red",
-        fillOpacity: 0.7,
-        radius: bishopric.attended ? 8 : 4, // Larger for attended
-    })
-    .bindTooltip(bishopric.name, { permanent: false, direction: "top" }) // Info box on hover
-    .addTo(map);
-});
-
-//add popups
-const infoBox = document.getElementById("info-box");
-
-bishoprics.forEach((bishopric) => {
-    const marker = L.circleMarker([bishopric.lat, bishopric.lon], {
-        color: bishopric.attended ? "green" : "red",
-        fillColor: bishopric.attended ? "green" : "red",
-        fillOpacity: 0.7,
-        radius: bishopric.attended ? 8 : 4,
-    }).addTo(map);
-
-    // Show info on hover
-    marker.on("mouseover", function () {
-        infoBox.style.display = "block";
-        infoBox.innerHTML = `<b>${bishopric.name}</b><br>Attended: ${bishopric.attended ? "Yes" : "No"}`;
-    });
-
-    // Hide info when the mouse leaves
-    marker.on("mouseout", function () {
-        infoBox.style.display = "none";
-    });
-});
-
-//add legend

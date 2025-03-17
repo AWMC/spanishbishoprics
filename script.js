1d30944b3f3934715fb093739aae3c0c5c830c0c
ompatel33 <ompatel@unc.edu>
2025-03-17 17:38:15 -0400

    Created colored attendance marker and adjusted code.

--- script.js
+++ script.js
@@ -1,50 +1,70 @@
-//LAUNCHING THE MAP
-//declare the map
+// LAUNCHING THE MAP
+// declare the map
 var map = L.map('map').setView([40, -5], 6);
  
 //add tile layer from CAWM
 L.tileLayer('https://cawm.lib.uiowa.edu/tiles/{z}/{x}/{y}.png', {}).addTo(map);
  
-//add geoJSON data
-fetch("attendance-geodata.geojson")
-    .then(response => {
-        if (!response.ok) return;
-        return response.json();
-})
-    .then(raw_data => {
-        L.geoJSON(raw_data, {
-            //add vector appearance
-            pointToLayer: function(feature, latlng) {
-                if (feature.properties.attendance == true) { //HELP! How do I reference all attendance when they have different names!!
+// set starting council to Elvira 306
+var selectedCouncil = 'Elvira 306';
+
+// Function to fetch and render GeoJSON data
+function renderGeoJSON() {
+    fetch("attendance-geodata.geojson")
+        .then(response => {
+            if (!response.ok) return;
+            return response.json();
+        })
+        .then(raw_data => {
+            L.geoJSON(raw_data, {
+                //add vector appearance
+                pointToLayer: function(feature, latlng) {
+                    var attendanceProperty = selectedCouncil + " Attendance";
+                    var attendance = feature.properties[attendanceProperty];
+            
+                    if (attendance == true) { 
+                        return L.circleMarker(latlng, {
+                            radius : 7,           // set size adjustment
+                            color : "green",      // set outline color
+                            fillColor : "green",  // set fill color
+                            fillOpacity : 0.5,    // set transparency
+                            weight : 1            // set outline thickness   
+                        });
+                    } else if (attendance == false) {
+                        return L.circleMarker(latlng, {
+                            radius : 5,         // set size adjustment
+                            color : "red",      // set outline color
+                            fillColor : "red",  // set fill color
+                            fillOpacity : 0.5,  // set transparency
+                            weight : 1          // set outline thickness
+                        });
+                    }
+                }, 
+                //add popups
+                onEachFeature: function(feature, layer) {  
+                    //customize popup content
+                    var popupContent = 
+                        `<b>${feature.properties.See}</b><br>
+                        Province: ${feature.properties.Province || "Unknown"}<br>
+                        Notes: ${feature.properties.Notes || "None"}`;
+                    layer.bindPopup(popupContent);
                 }
-                return L.circleMarker(latlng, {
-                    radius : 5,         //set size adjustment
-                    color : "black",    //set outline color
-                    fillColor : "black",//set fill color
-                    fillOpacity : 0.5,  //set transparency
-                    weight : 1          //set outline thickness   
-                });
-            }, 
-            //add popups
-            onEachFeature: function(feature, layer) {  
-                //customize popup content
-                var popupContent = 
-                    `<b>${feature.properties.See}</b><br>
-                    Province: ${feature.properties.Province || "Unknown"}<br>
-                    Notes: ${feature.properties.Notes || "None"}`;
-                layer.bindPopup(popupContent);
-        }}).addTo(map);
+            }).addTo(map);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
 
-        //set starting council to Elivra 306
-        var selectedCouncil = 'Elvira 306';
+// fetch and render
+renderGeoJSON();
 
-        //declare custom control
-        var councilSelectorMenu = L.control.custom({
-        position: 'topright',
-        content: '<div class="custom-control">' +
-        '<h1>Select Council</h1>' +
-        '<br>'+
-        '<select onchange="handleDropdownChange(this)">' +
+// declare custom control
+var councilSelectorMenu = L.control({position: 'topright'});
+councilSelectorMenu.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'custom-control');
+    div.innerHTML = '<h1>Select Council</h1>' +
+        '<select id="councilSelector">' +
         '<option value="Elvira 306">Elvira 306</option>' +  
         '<option value="Zaragoza 380">Zaragoza 380</option>' +
         '<option value="Toledo 400">Toledo 400</option>' +
@@ -57,22 +77,19 @@ fetch("attendance-geodata.geojson")
         '<option value="Braga 561">Braga 561</option>' +
         '<option value="Braga 572">Braga 572</option>' +
         '<option value="Toledo 589">Toledo 589</option>' +
-        '</select>' +
-        '</div>',
-        classes: 'custom-control'
-        });
+        '</select>';
+    div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
+    return div;
+};
+councilSelectorMenu.addTo(map);
 
-        // Add the custom control to the map
-        councilSelectorMenu.addTo(map);
-
-        function handleDropdownChange(select) {
-            //reset selectedCouncil
-            selectedCouncil = select.value;    
+// Handle dropdown change
+document.getElementById('councilSelector').addEventListener('change', function(e) {
+    selectedCouncil = e.target.value;
+    map.eachLayer(function (layer) {
+        if (layer instanceof L.GeoJSON) {
+            map.removeLayer(layer);
         }
-            })    
-        .catch(error => {
-            console.error(error);})
-
-        //HANDLEDROPDOWNCHANGE
-        //define the handleDropdownChange function
-        
+    });
+    renderGeoJSON();
+});

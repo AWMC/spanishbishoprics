d5f885dec384defbe97d5b40bf78be0b65132a8f
ompatel33 <ompatel@unc.edu>
2025-03-26 17:12:14 -0400

    Added Plugin and Code Edit
    Added FontAwesome for symbology customization. Corrected GeoJSON data.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var map = L.map('map').setView([40, -5], 6);
 L.tileLayer('https://cawm.lib.uiowa.edu/tiles/{z}/{x}/{y}.png', {}).addTo(map);
  
 // set starting council to Elvira 306
-var selectedCouncil = 'Elvira 306';
+var selectedCouncil = 'Elvira_306';
 
 // function to fetch and render GeoJSON data
 function renderGeoJSON() {
@@ -17,7 +17,7 @@ function renderGeoJSON() {
         })
         .then(raw_data => {
             L.geoJSON(raw_data, {
-                // add vector appearance
+                // add vector appearance (consider disability access)
                 pointToLayer: function(feature, latlng) {
                     var attendanceProperty = selectedCouncil + " Attendance";
                     var attendance = feature.properties[attendanceProperty];
@@ -50,7 +50,7 @@ function renderGeoJSON() {
                         `<b>${feature.properties.See || "Unknown Location"}</b><br>
                         Modern City: ${feature.properties.Modern_City || "Unknown"}<br>
                         Province: ${feature.properties.Province || "Unknown"}<br>
-                        Bishop Attended: ${feature.properties.selectedCouncil || "None"}<br>
+                        Bishop Attended: ${feature.properties.Elvira_306 || "None"}<br>
                         Earliest Attested Bishop: ${feature.properties.Earliest_Attested_Bishop || "Unknown"}<br>
                         Notes: ${feature.properties.Notes || "None"}`;
                     layer.bindPopup(popupContent);
@@ -80,18 +80,18 @@ councilSelectorMenu.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'custom-control');
     div.innerHTML = '<h1>Select Council</h1>' +
         '<select id="councilSelector">' +
-        '<option value="Elvira 306">Elvira 306</option>' +  
-        '<option value="Zaragoza 380">Zaragoza 380</option>' +
-        '<option value="Toledo 400">Toledo 400</option>' +
-        '<option value="Tarragona 516">Tarragona 516</option>' +
-        '<option value="Giron 517">Giron 517</option>' +
-        '<option value="Toledo 527">Toledo 527</option>' +
-        '<option value="Barcelona 540">Barcelona 540</option>' +
-        '<option value="Lérida 546">Lérida 546</option>' +
-        '<option value="Valencia 546">Valencia 546</option>' +
-        '<option value="Braga 561">Braga 561</option>' +
-        '<option value="Braga 572">Braga 572</option>' +
-        '<option value="Toledo 589">Toledo 589</option>' +
+        '<option value="Elvira_306">Elvira 306</option>' +  
+        '<option value="Zaragoza_380">Zaragoza 380</option>' +
+        '<option value="Toledo_400">Toledo 400</option>' +
+        '<option value="Tarragona_516">Tarragona 516</option>' +
+        '<option value="Giron_517">Giron 517</option>' +
+        '<option value="Toledo_527">Toledo 527</option>' +
+        '<option value="Barcelona_540">Barcelona 540</option>' +
+        '<option value="Lérida_546">Lérida 546</option>' +
+        '<option value="Valencia_546">Valencia 546</option>' +
+        '<option value="Braga_561">Braga 561</option>' +
+        '<option value="Braga_572">Braga 572</option>' +
+        '<option value="Toledo_589">Toledo 589</option>' +
         '</select>';
     div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
     return div;
@@ -100,51 +100,8 @@ councilSelectorMenu.addTo(map);
 
 // handle dropdown change
 document.getElementById('councilSelector').addEventListener('change', function(e) {
-    const selectedValue = e.target.value;
-    selectedCouncil = selectedValue;
-    // use a switch statement to handle council selection
-    switch (selectedValue) {
-        case 'Elvira 306':
-            selectedCouncil = 'Elvira 306';
-            break;
-        case 'Zaragoza 380':
-            selectedCouncil = 'Zaragoza 380';
-            break;
-        case 'Toledo 400':
-            selectedCouncil = 'Toledo 400';
-            break;
-        case 'Tarragona 516':
-            selectedCouncil = 'Tarragona 516';
-            break;
-        case 'Giron 517':
-            selectedCouncil = 'Giron 517';
-            break;
-        case 'Toledo 527':
-            selectedCouncil = 'Toledo 527';
-            break;
-        case 'Barcelona 540':
-            selectedCouncil = 'Barcelona 540';
-            break;
-        case 'Lérida 546':
-            selectedCouncil = 'Lérida 546';
-            break;
-        case 'Valencia 546':
-            selectedCouncil = 'Valencia 546';
-            break;
-        case 'Braga 561':
-            selectedCouncil = 'Braga 561';
-            break;
-        case 'Braga 572':
-            selectedCouncil = 'Braga 572';
-            break;
-        case 'Toledo 589':
-            selectedCouncil = 'Toledo 589';
-            break;
-        // exit the function if an unknown council is selected
-        default:
-            console.error('Unknown council selected:', selectedValue);
-            return;
-    }
+    selectedCouncil = e.target.value;
+
    clearLayers(); 
    renderGeoJSON();
 });
